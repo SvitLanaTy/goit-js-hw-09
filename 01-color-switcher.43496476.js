87a34598b6a7936520fb1ca02978d38c41b8d1f4
SvitLanaTy <114913397+SvitLanaTy@users.noreply.github.com>
2023-05-04 07:07:07 +0000

    Deploying to gh-pages from @ SvitLanaTy/goit-js-hw-09@be5ee602f5c7965280d6a9014e575fc6ce56683f 🚀

---
+++ 01-color-switcher.43496476.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]"),n=document.querySelector("body");function o(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));n.style.backgroundColor=t}e.addEventListener("click",(function(){t=setInterval(o,1e3),e.disabled=!0})),a.addEventListener("click",(function(){clearInterval(t),e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.43496476.js.map
