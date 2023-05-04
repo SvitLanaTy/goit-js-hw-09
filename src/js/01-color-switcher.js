be5ee602f5c7965280d6a9014e575fc6ce56683f
SvitLanaTy <stymchenko@gmail.com>
2023-05-04 09:06:32 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector(`body`);
 
+let timer;
+
+function changeBgColor() {
+  let bgColor = getRandomHexColor();
+  body.style.backgroundColor = bgColor;
+}
+
+function onStartClick() {
+  timer = setInterval(changeBgColor, 1000);
+  startBtn.disabled = true;
+}
+function onStopClick() {
+  clearInterval(timer);
+  startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startBtn.addEventListener(`click`, onStartClick);
+stopBtn.addEventListener(`click`, onStopClick);
